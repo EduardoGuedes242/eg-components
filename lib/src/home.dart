202483744d2145610d3d389eb22aca13c1a2f738
EduardoGuedes242 <eduardoguedesdev@gmail.com>
2024-03-11 21:51:08 -0300

    Atualização do preojeto

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -12,63 +12,7 @@ class HomePage extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              height: 76,
-              width: 327,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Input Label',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      color: Colors.white,
-                      fontSize: 16,
-                    ),
-                  ),
-                  Spacer(),
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      color: Color(0XFF202325),
-                    ),
-                    height: 48,
-                    width: 327,
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        hintText: 'Nome',
-                        hintStyle: TextStyle(
-                          color: Color(0XFF404446),
-                          fontSize: 16,
-                        ),
-                        isCollapsed: false,
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                          borderSide: BorderSide(color: Colors.transparent),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                          borderSide: BorderSide(color: Colors.transparent),
-                        ),
-                        errorBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                          borderSide: BorderSide(color: Colors.red),
-                        ),
-                        focusedErrorBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                          borderSide: BorderSide(color: Colors.red),
-                        ),
-                      ),
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
+          children: [],
         ),
       ),
     );
